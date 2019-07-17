b1132cf5256a82d84ebc34ca6e6941fc8579a123
Kyle Salstrom <kylesalstrom@gmail.com>
2019-07-17 01:02:14 -0700

    broken but beautiful

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import moment from "moment";
+//import moment from "moment";
 
 function App() {
   console.log('lets go')
@@ -20,39 +20,98 @@ class WheelWindow extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      wheelMovements: [],
-      swingState: 0
+      wheelMovements: {
+        backswing: [],
+        topswing: 0,
+        downswing: [],
+      },
+      swingState: 0,
+      paceCounter: 0,
+      intervalId: 0,
     }
   }
 
+  startSwing = () => {
+    clearInterval(this.state.intervalId)
+    const newId = setInterval(() => this.paceTick, 1)
+    this.setState({
+      intervalId: newId
+    }) 
+  }
+  endSwing = () => {
+    clearInterval(this.state.intervalId)
+  }
+  paceTick = () => {
+    this.setState((state) => {
+      return (state.swingState !== 0 || state.swingState !== 2) && state.paceCounter > 100 ?
+        { paceCounter: 0, swingState: state.swingState + 1, } :
+        { paceCounter: state.paceCounter + 1} 
+    })
+  }
+  
+  handleClick() {
+    this.setState({
+      swingState: 0,
+      paceCounter: 0,
+    })
+  }
   handleScroll = (e) => {
-    const { deltaY, type} = e
-    if (type === 'wheel') {
-      this.setState((state) => {
-        return { wheelMovements: state.wheelMovements.slice(0, state.wheelMovements.length).concat([{ time: moment(), movement: deltaY }]) }
-      })
+    console.log(this.state)
+    if (this.state.swingState === 0) {
+      this.startSwing()
     }
+    // const { deltaY, type } = e
+    this.setState((state) => {
+      switch (state.swingState) {
+        case 0: //triggers backswing //existing state had not experienced mosewheel movement until now, 
+             return {
+               swingState: 1,
+               paceCounter: 0,
+             }
+           case 1: //backswing in progress
+             return {
+              wheelMovements: {
+                 backswing: state.wheelMovements.backswing.slice(0, state.wheelMovements.backswing.length).concat([state.paceCounter])
+               },
+             }
+           case 2: //triggers downswing //existing state had been holding at topswing, 
+             return {
+               wheelMovements: {
+                 topswing: state.paceCounter,
+                 swingState: 3,
+                 paceCounter: 0,
+               },
+             }
+           case 3: //downswing in progress
+             return {
+               wheelMovements: {
+                 foreswing: state.wheelMovements.foreswing.slice(0, state.wheelMovements.foreswing.length).concat([state.paceCounter])
+               },
+               paceCounter: 0,
+             }
+        default: //swing complete, kick back to ball in hand
+             this.endSwing()
+             return {
+              swingState: -1,
+              paceCounter: 0,
+             }
+         }
+      })
   }
 
   render() {
     console.log(this.state.wheelMovements[this.state.wheelMovements.length - 1])
-    return this.state.wheelMovements && this.state.wheelMovements.length > 0 ?
+    return this.state.swingState > 0 ?
       (
         <button onWheel={this.handleScroll}>
-          <h1>{this.state.wheelMovements[this.state.wheelMovements.length - 1].movement}</h1>
+          <h1>{this.state.paceCounter}</h1>
         </button>
       )
         :
       (
       <button onWheel={this.handleScroll}>
-        <h1>Hello, world!</h1>
+        <h1>x                  x</h1>
       </button>
     );
   }
-}
-
-
-
-class timer {
-
 }
