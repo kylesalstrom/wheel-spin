d94ad85d4efe558506beedf133cce7b6817266c3
Kyle Salstrom <kylesalstrom@gmail.com>
2019-07-19 01:03:14 -0700

    new mouse who dis

--- src/App.js
+++ src/App.js
@@ -20,14 +20,13 @@ class WheelWindow extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      wheelMovements: {
-        backswing: [],
-        topswing: 0,
-        downswing: [],
-      },
+      backswing: [],
+      topswing: 0,
+      downswing: [],
       swingState: -2,
       paceCounter: 0,
       intervalId: 0,
+      isBackswingDeltaPostive: false
     }
   }
 
@@ -43,7 +42,7 @@ class WheelWindow extends React.Component {
   }
   paceTick = () => {
     this.setState((state) => {
-      if (state.paceCounter < 20) {
+      if (state.paceCounter < 50) {
         return { paceCounter: state.paceCounter + 1 } 
       }
       else {
@@ -64,57 +63,52 @@ class WheelWindow extends React.Component {
   
   handleClick = () => {
     this.setState({
-      wheelMovements: {
-        backswing: [],
-        topswing: 0,
-        downswing: [],
-      },
+      backswing: [],
+      topswing: 0,
+      downswing: [],
       swingState: 0,
       paceCounter: 0,
       intervalId: 0,
+      isBackswingDeltaPostive: false
     })
   }
   handleScroll = (e) => {
+    console.log('------------------')
+    const thisDir = 0 < e.deltaY
+    console.log(thisDir)
     if (this.state.swingState === 0) {
       this.startSwing()
     }
-    // const { deltaY, type } = e
+   console.log(e.deltaY)
     this.setState((state) => {
       switch (state.swingState) {
         case 0: //triggers backswing //existing state had not experienced mosewheel movement until now, 
              return {
                swingState: 1,
                paceCounter: 0,
+               isBackswingDeltaPostive: thisDir
              }
-           case 1: //backswing in progress
+        case 1: //backswing in progress
+        case 2: //antiquated
+          console.log('backswing in progress')
+          return thisDir === state.isBackswingDeltaPostive ?
+            {
+              backswing: state.backswing.slice(0, state.backswing.length).concat([state.paceCounter]),
+              paceCounter: 0,
+            } :
+            {
+              topswing: state.paceCounter,
+              swingState: 3,
+              paceCounter: 0,
+             }
+        case 3: //downswing in progress
+          console.log('downswing in progress')
              return {
-              wheelMovements: {
-                 backswing: state.wheelMovements.backswing.slice(0, state.wheelMovements.backswing.length).concat([state.paceCounter]),
-                 topswing: state.wheelMovements.topswing,
-                 downswing: state.wheelMovements.downswing,
-               },
-               paceCounter: 0,
-             }
-           case 2: //triggers downswing //existing state had been holding at topswing, 
-             return {
-               wheelMovements: {
-                 backswing: state.wheelMovements.backswing,
-                 topswing: state.paceCounter,
-                 downswing: state.wheelMovements.downswing,
-               },
-               swingState: 3,
-               paceCounter: 0,
-             }
-           case 3: //downswing in progress
-             return {
-               wheelMovements: {
-                 backswing: state.wheelMovements.backswing,
-                 topswing: state.wheelMovements.topswing,
-                 downswing: state.wheelMovements.downswing.slice(0, state.wheelMovements.downswing.length).concat([state.paceCounter])
-               },
-               paceCounter: 0,
-             }
+              downswing: state.downswing.slice(0, state.downswing.length).concat([state.paceCounter]),
+              paceCounter: 0,
+          }
         default: //swing complete, kick back to ball in hand
+          console.log('swing complete, kick back to ball in hand')
              this.endSwing()
              return {
               swingState: -1,
@@ -127,13 +121,13 @@ class WheelWindow extends React.Component {
   buttonText = () => {
     switch (this.state.swingState) {
       case -2:
-        return 'x               x'
+        return 'x             x'
       case -1:
-        return Math.max(60 - this.state.wheelMovements.backswing.length, (95 + this.state.wheelMovements.downswing.length) - (Math.abs(this.state.wheelMovements.topswing - 50)/2))
+        return Math.max(60 - this.state.backswing.length, (95 + this.state.downswing.length) - (Math.abs(this.state.topswing - 50)/2))
           + " | "
-          + (this.state.wheelMovements.backswing.reduce((a, b) => a + b, 0) - this.state.wheelMovements.downswing.reduce((a, b) => a + b, 0))
+          + (this.state.backswing.reduce((a, b) => a + b, 0) - this.state.downswing.reduce((a, b) => a + b, 0))
           + " | "
-          + (this.state.wheelMovements.backswing.length - this.state.wheelMovements.downswing.length)
+          + (this.state.backswing.length - this.state.downswing.length)
       default:
         return this.state.paceCounter;
     }
@@ -145,23 +139,23 @@ class WheelWindow extends React.Component {
     <div className="course" >
       <div>
         <div>
-          {this.state.wheelMovements.backswing.map((n, index) => {
+          {this.state.backswing.map((n, index) => {
             return <h6 className="detailText" key={index}>{n}</h6>
           })}
         </div>
         <div>
           <div >
-              <h3 className="flankHeading">{this.state.wheelMovements.backswing.reduce((a, b) => a + b, 0)}</h3>
+              <h3 className="flankHeading">{this.state.backswing.reduce((a, b) => a + b, 0)}</h3>
           </div>
           <div>
-              <h2 className="centerHeading"> {this.state.wheelMovements.topswing}</h2>
+              <h2 className="centerHeading"> {this.state.topswing}</h2>
           </div>
           <div>
-              <h3 className="flankHeading">{this.state.wheelMovements.downswing.reduce((a, b) => a + b, 0)}</h3>
+              <h3 className="flankHeading">{this.state.downswing.reduce((a, b) => a + b, 0)}</h3>
           </div>
         </div>
         <div>
-          {this.state.wheelMovements.downswing.map((n, index) => {
+          {this.state.downswing.map((n, index) => {
             return <h6 className="detailText" key={index}>{n}</h6>
           })}
         </div>
